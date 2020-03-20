12084f16ae67215859a90363cec550b15bcc213c
Oleksandr Roienko <OleksandrRoienko@epam.com>
2020-03-20 22:45:13 +0200

    Initial commit

---
+++ src/test/java/com/robosh/ejournal/EJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robosh.ejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
