5512ebd6e33ddbac043fa1038e82914b53ad7584
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-27 16:31:45 +0200

    added @Entity and id for Subject

--- src/main/java/com/robosh/ejournal/entity/Subject.java
+++ src/main/java/com/robosh/ejournal/entity/Subject.java
@@ -1,5 +1,13 @@
 package com.robosh.ejournal.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Subject {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
 }
