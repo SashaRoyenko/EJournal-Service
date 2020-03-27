4950d11c8c184b32792ca039fdad8b5693a91a60
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-27 16:48:46 +0200

    changed Student`s Parent to List<Parents>

--- src/main/java/com/robosh/ejournal/entity/Student.java
+++ src/main/java/com/robosh/ejournal/entity/Student.java
@@ -1,14 +1,18 @@
 package com.robosh.ejournal.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Student extends User {
+
     private LocalDate dateOfBirth;
 
     private Group group;
 
-    private Parent parent;
+    private List<Parent> parents;
 
 }
