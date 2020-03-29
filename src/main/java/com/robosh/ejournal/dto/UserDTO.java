b35741df9d5fd613a7070052c5757ecc26b2f83a
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-29 20:43:34 +0300

    create UserDTO class

---
+++ src/main/java/com/robosh/ejournal/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.robosh.ejournal.dto;
+
+import com.robosh.ejournal.entity.School;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    protected Long id;
+
+    protected String firstName;
+
+    protected String secondName;
+
+    protected String lastName;
+
+    protected String email;
+
+    protected String password;
+
+    protected String phone;
+
+    protected School school;
+}
