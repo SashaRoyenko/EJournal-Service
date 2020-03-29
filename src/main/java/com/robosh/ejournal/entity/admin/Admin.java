a4d1f0e0f26e64edfbbe52481328700fef58675d
Oleksandr Roienko <sasharoyenko@gmail.com>
2020-03-29 13:55:26 +0300

    Fix pull request issues

--- src/main/java/com/robosh/ejournal/entity/admin/Admin.java
+++ src/main/java/com/robosh/ejournal/entity/admin/Admin.java
@@ -13,6 +13,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -21,32 +22,36 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
+@Entity(name = "Admin")
 public class Admin {
+
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
 
     @NotBlank
-    @Column(nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @NotBlank
-    @Column(nullable = false)
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Pattern(regexp = "[A-Za-z0-9+_.-]+@[a-z.-]+\\.[a-z]{2,8}")
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @NotBlank
-    @Column(nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Nullable
     @ManyToOne
+    @JoinColumn(name = "school_id", nullable = false)
     private School school;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
     private AdminRole adminRole;
 }
