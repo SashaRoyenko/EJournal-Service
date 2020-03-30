425f2a371b3ecd8695d2d34242c7205f0a7bafc2
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-30 12:37:35 +0300

    add AdminDTO

---
+++ src/main/java/com/robosh/ejournal/dto/AdminDTO.java
@@ -0,0 +1,19 @@
+package com.robosh.ejournal.dto;
+
+import com.robosh.ejournal.entity.School;
+import com.robosh.ejournal.entity.admin.AdminRole;
+
+public class AdminDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private School school;
+
+    private AdminRole adminRole;
+}
