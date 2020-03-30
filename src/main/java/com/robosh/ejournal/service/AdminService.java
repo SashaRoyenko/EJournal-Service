42dbcbdbf759f24c99067df891c07e397a9c55df
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-30 12:32:55 +0300

    add AdminRepository dependency in AdminService

--- src/main/java/com/robosh/ejournal/service/AdminService.java
+++ src/main/java/com/robosh/ejournal/service/AdminService.java
@@ -1,8 +1,13 @@
 package com.robosh.ejournal.service;
 
+import com.robosh.ejournal.repository.AdminRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
+    private final AdminRepository adminRepository;
 
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
 }
