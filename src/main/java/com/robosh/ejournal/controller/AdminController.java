752b5b7bd17d6bf03f8c6fb1bb52a78753f5d0e1
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-30 13:12:12 +0300

    add AdminService dependency in AdminController and implemented method getAll

--- src/main/java/com/robosh/ejournal/controller/AdminController.java
+++ src/main/java/com/robosh/ejournal/controller/AdminController.java
@@ -1,8 +1,23 @@
 package com.robosh.ejournal.controller;
 
+import com.robosh.ejournal.dto.AdminDTO;
+import com.robosh.ejournal.service.AdminService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AdminController {
 
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @RequestMapping("/admins")
+    public List<AdminDTO> getAll() {
+        return adminService.getAllAdmins();
+    }
 }
