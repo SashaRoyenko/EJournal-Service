61fe60781ca90b4fba631490aefa67a68ed3f61f
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-30 13:09:34 +0300

    realize getAllAdmins method to AdminService

--- src/main/java/com/robosh/ejournal/service/AdminService.java
+++ src/main/java/com/robosh/ejournal/service/AdminService.java
@@ -1,13 +1,25 @@
 package com.robosh.ejournal.service;
 
+import com.robosh.ejournal.dto.AdminDTO;
+import com.robosh.ejournal.mapper.AdminMapper;
 import com.robosh.ejournal.repository.AdminRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AdminService {
+
     private final AdminRepository adminRepository;
 
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
+    }
+
+    public List<AdminDTO> getAllAdmins() {
+        return adminRepository.findAll().stream()
+                .map(AdminMapper.ADMIN_MAPPER::fromAdminToAdminTDO)
+                .collect(Collectors.toList());
     }
 }
