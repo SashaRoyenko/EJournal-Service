da3d1fb910a7abde822b41eed7e314cb70d734de
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-03-30 13:08:07 +0300

    add AdminMapper

---
+++ src/main/java/com/robosh/ejournal/mapper/AdminMapper.java
@@ -0,0 +1,14 @@
+package com.robosh.ejournal.mapper;
+
+import com.robosh.ejournal.dto.AdminDTO;
+import com.robosh.ejournal.entity.admin.Admin;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AdminMapper {
+
+    AdminMapper ADMIN_MAPPER = Mappers.getMapper(AdminMapper.class);
+
+    AdminDTO fromAdminToAdminTDO(Admin admin);
+}
