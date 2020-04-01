a1603c852c33d747b592b0e8395f579d34407ae6
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-01 13:53:11 +0300

    refactor Admin Controller

--- src/main/java/com/robosh/ejournal/controller/AdminController.java
+++ src/main/java/com/robosh/ejournal/controller/AdminController.java
@@ -5,18 +5,20 @@ import com.robosh.ejournal.service.AdminService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
+@RequestMapping("/admins")
+@RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
 
     @ApiOperation(value = "Returns all admins")
-    @GetMapping("/admins")
+    @GetMapping
     public List<AdminInfoDTO> getAll() {
         return adminService.getAllAdmins();
     }
