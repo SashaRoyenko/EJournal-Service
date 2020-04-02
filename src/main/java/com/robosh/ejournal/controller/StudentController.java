630c7a2f68847359c9bd64c55f80126bb094b491
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-02 21:45:11 +0300

    add ExceptionHandler

--- src/main/java/com/robosh/ejournal/controller/StudentController.java
+++ src/main/java/com/robosh/ejournal/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.robosh.ejournal.controller;
 
 import com.robosh.ejournal.data.dto.student.StudentDto;
 import com.robosh.ejournal.data.entity.Student;
+import com.robosh.ejournal.exception.ResourceNotFoundException;
 import com.robosh.ejournal.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @ExceptionHandler({ResourceNotFoundException.class})
     @GetMapping("/{id}")
     public StudentDto getStudentById(@PathVariable Long id){
         return studentService.findById(id);
