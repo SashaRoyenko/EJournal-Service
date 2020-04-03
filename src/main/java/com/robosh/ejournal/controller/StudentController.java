77f55be342b97b868fbaf592675247738ec1f056
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-03 12:42:46 +0300

    remove exception handler

--- src/main/java/com/robosh/ejournal/controller/StudentController.java
+++ src/main/java/com/robosh/ejournal/controller/StudentController.java
@@ -2,13 +2,11 @@ package com.robosh.ejournal.controller;
 
 import com.robosh.ejournal.data.dto.student.StudentDto;
 import com.robosh.ejournal.data.dto.student.StudentSaveDto;
-import com.robosh.ejournal.exception.ResourceNotFoundException;
 import com.robosh.ejournal.service.StudentService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +22,6 @@ public class StudentController {
     private final StudentService studentService;
 
     @ApiOperation("Get student by Id")
-    @ExceptionHandler({ResourceNotFoundException.class})
     @GetMapping("/{id}")
     public StudentDto getStudentById(@PathVariable Long id){
         return studentService.findById(id);
