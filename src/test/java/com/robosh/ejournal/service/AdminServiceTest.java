b0657eb0656207a36f96efefce737f255ae3b40e
nikitabortnichuk <nikita.bortnichuk41@gmail.com>
2020-04-03 17:05:34 +0300

    add test for update and for findById

--- src/test/java/com/robosh/ejournal/service/AdminServiceTest.java
+++ src/test/java/com/robosh/ejournal/service/AdminServiceTest.java
@@ -5,10 +5,14 @@ import com.robosh.ejournal.data.dto.admin.SaveAdminDto;
 import com.robosh.ejournal.data.entity.admin.Admin;
 import com.robosh.ejournal.data.entity.admin.AdminRole;
 import com.robosh.ejournal.data.repository.AdminRepository;
+import com.robosh.ejournal.exception.ResourceNotFoundException;
 import config.MapperConfiguration;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,6 +22,7 @@ import javax.validation.ValidationException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static com.robosh.ejournal.data.DummyData.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,7 +34,8 @@ import static org.mockito.Mockito.when;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {
         AdminService.class,
-        MapperConfiguration.class
+        MapperConfiguration.class,
+        ModelMapper.class
 })
 class AdminServiceTest {
 
@@ -49,10 +55,27 @@ class AdminServiceTest {
     }
 
     @Test
+    void Should_ReturnAdminInfoDtoById_When_FindById(){
+        when(mockedAdminRepository.findById(any())).thenReturn(Optional.of(getAdmin()));
+
+        Admin expected = getAdmin();
+        Admin actual = adminService.findById(any());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void Should_ThrowResourceNotFoundException_WhenAdminNotFound_ForFindById(){
+        ResourceNotFoundException resourceNotFoundException =
+                assertThrows(ResourceNotFoundException.class, () -> adminService.findById(any()));
+        assertEquals("Admin not found with id : 'null'", resourceNotFoundException.getMessage());
+    }
+
+    @Test
     void Should_SaveAdmin_WhenDataValid() {
         when(mockedAdminRepository.save(any())).thenReturn(getAdmin());
 
-        SaveAdminDto adminToSave = getUpdateAdminDto();
+        SaveAdminDto adminToSave = getSaveAdminDto();
         AdminInfoDto result = adminService.save(adminToSave);
         AdminInfoDto expected = getAdminInfoDto();
 
@@ -62,10 +85,35 @@ class AdminServiceTest {
 
     @Test
     void Should_ThrowValidationException_WhenPasswordNotEquals_ForSaveAdmin() {
-        SaveAdminDto adminToSave = getUpdateAdminDto();
+        SaveAdminDto adminToSave = getSaveAdminDto();
         adminToSave.setConfirmedPassword("notsamepassword");
         ValidationException validationException = assertThrows(ValidationException.class, () -> adminService.save(adminToSave));
         assertEquals("Password should be same", validationException.getMessage());
+    }
+
+    @Test
+    void Should_UpdateAdmin_WhenDataValid() {
+        when(mockedAdminRepository.findById(any())).thenReturn(Optional.of(getAdmin()));
+        when(mockedAdminRepository.save(any())).thenReturn(getAdmin());
+
+        SaveAdminDto adminToSave = getSaveAdminDto();
+        AdminInfoDto result = adminService.update(adminToSave);
+        AdminInfoDto expected = getAdminInfoDto();
+
+        assertEquals(expected, result);
+        verify(mockedAdminRepository).save(any());
+    }
+
+    @Test
+    void Should_ThrowValidationException_WhenPasswordNotEquals_ForUpdateAdmin() {
+        SaveAdminDto adminToSave = getSaveAdminDto();
+        adminToSave.setConfirmedPassword("notsamepassword");
+        ValidationException validationException = assertThrows(ValidationException.class, () -> adminService.update(adminToSave));
+        assertEquals("Password should be same", validationException.getMessage());
+    }
+
+    private void givenAdmins() {
+        adminsList = getAdmins();
     }
 
     private void whenGetAllAdmins() {
@@ -75,10 +123,6 @@ class AdminServiceTest {
     private void thenShouldReturn() {
         List<AdminInfoDto> actualAdminDTOS = adminService.findAll();
         assertEquals(getExpectedAdminInfoDTOs(), actualAdminDTOS);
-    }
-
-    private void givenAdmins() {
-        adminsList = getAdmins();
     }
 
     private List<Admin> getAdmins() {
@@ -142,7 +186,7 @@ class AdminServiceTest {
                 .build();
     }
 
-    private SaveAdminDto getUpdateAdminDto() {
+    private SaveAdminDto getSaveAdminDto() {
         return SaveAdminDto.builder()
                 .firstName(NAME)
                 .lastName(NAME)
