8fdf1b9e6eac99cff6658cd9bbbc2e829116d794
Oleksandr Roienko <sasharoyenko@gmail.com>
2020-04-04 17:19:38 +0300

    Change vay of validation in controller

--- src/main/java/com/robosh/ejournal/controller/AdminController.java
+++ src/main/java/com/robosh/ejournal/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.robosh.ejournal.controller;
 import com.robosh.ejournal.data.dto.admin.AdminInfoDto;
 import com.robosh.ejournal.data.dto.admin.SaveAdminDto;
 import com.robosh.ejournal.service.AdminService;
+import com.robosh.ejournal.util.validation.ValidatorProcessor;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +27,8 @@ public class AdminController {
 
     @ApiOperation(value = "Add an admin")
     @PostMapping
-    public ResponseEntity<AdminInfoDto> saveAdmin(@ApiParam @RequestBody @Valid SaveAdminDto saveAdminDto){
+    public ResponseEntity<AdminInfoDto> saveAdmin(@ApiParam @RequestBody SaveAdminDto saveAdminDto){
+        ValidatorProcessor.validate(saveAdminDto);
         AdminInfoDto adminDto = adminService.save(saveAdminDto);
         return new ResponseEntity<>(adminDto, HttpStatus.CREATED);
     }
