2a6f1fd1781d3806a9ce814c8f13a77e86e406bd
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-06 14:18:38 +0300

    add log

--- src/main/java/com/robosh/ejournal/service/StudentService.java
+++ src/main/java/com/robosh/ejournal/service/StudentService.java
@@ -7,29 +7,33 @@ import com.robosh.ejournal.data.mapping.StudentMapper;
 import com.robosh.ejournal.data.repository.StudentRepository;
 import com.robosh.ejournal.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class StudentService {
 
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
 
-    public StudentDto findById(Long id){
-            return studentMapper.fromStudentToStudentDto(
-                    studentRepository.findById(id)
-                    .orElseThrow(()->new ResourceNotFoundException("Student", "id", id)));
+    public StudentDto findById(Long id) {
+        return studentMapper.fromStudentToStudentDto(
+                studentRepository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("Student", "id", id)));
     }
 
-    public StudentDto save(SaveStudentDto dto){
+    public StudentDto save(SaveStudentDto dto) {
         dto.setParents(Optional.ofNullable(dto.getParents()).orElse(new ArrayList<>()));
-        Student student = studentMapper.fromStudentSaveDtoToStudent(dto);
-        return studentMapper.fromStudentToStudentDto(
-                studentRepository.save(student)
+        StudentDto studentDto = studentMapper.fromStudentToStudentDto(
+                studentRepository.save(
+                        studentMapper.fromStudentSaveDtoToStudent(dto))
         );
+        log.info("Student saved");
+        return studentDto;
     }
 }
