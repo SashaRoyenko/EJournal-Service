54e1e46c8a199042e7a365e90ddab358afd829e5
nikitabortnichuk <nikita.bortnichuk41@gmail.com>
2020-04-08 18:00:10 +0300

    add method findById which returns AdminInfoDto

--- src/main/java/com/robosh/ejournal/service/AdminService.java
+++ src/main/java/com/robosh/ejournal/service/AdminService.java
@@ -37,7 +37,7 @@ public class AdminService {
 
     public AdminInfoDto update(SaveAdminDto updateAdminDto) {
 
-        Admin currentAdmin = findById(updateAdminDto.getId());
+        Admin currentAdmin = findAdminById(updateAdminDto.getId());
         Admin updateAdmin = adminMapper.fromSaveAdminDtoToAdmin(updateAdminDto);
 
         saveSchoolForAdmin(updateAdminDto, updateAdmin);
@@ -53,7 +53,11 @@ public class AdminService {
         return adminMapper.fromAdminsToAdminsInfoDto(adminRepository.findAll());
     }
 
-    public Admin findById(Long id) {
+    public AdminInfoDto findById(Long id){
+        return adminMapper.fromAdminToAdminInfoDto(findAdminById(id));
+    }
+
+    private Admin findAdminById(Long id) {
         return adminRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Admin", "id", id));
     }
