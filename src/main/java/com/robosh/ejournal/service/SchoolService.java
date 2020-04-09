b2ccab78f147f51cfaeef5acf587bd147fc142f5
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-09 14:36:41 +0300

    create repository and service

---
+++ src/main/java/com/robosh/ejournal/service/SchoolService.java
@@ -0,0 +1,18 @@
+package com.robosh.ejournal.service;
+
+import com.robosh.ejournal.data.repository.SchoolRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class SchoolService {
+
+    private final SchoolRepository schoolRepository;
+
+//    public SchoolInfoDto save(SaveSchoolDto saveSchoolDto){
+
+   // }
+}
