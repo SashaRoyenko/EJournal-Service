af0d6e7909a42a57bb5fe953b0f81f92b9e9e021
Oleksandr Roienko <sasharoyenko@gmail.com>
2020-04-10 16:47:05 +0300

    Create password generator and add to save admin and student

--- src/main/java/com/robosh/ejournal/config/BeanConfig.java
+++ src/main/java/com/robosh/ejournal/config/BeanConfig.java
@@ -3,10 +3,13 @@ package com.robosh.ejournal.config;
 
 import com.robosh.ejournal.factory.BeansFactory;
 import com.robosh.ejournal.service.ValidationService;
+import com.robosh.ejournal.util.PasswordGenerator;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.robosh.ejournal.constant.Constants.PASSWORD_LENGTH;
 
 @Configuration
 public class BeanConfig {
@@ -16,6 +19,17 @@ public class BeanConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setSkipNullEnabled(true).setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordGenerator getPasswordGenerator() {
+        return PasswordGenerator.builder()
+                .isAllowChars(true)
+                .isAllowNumber(true)
+                .isAllowUpperLetters(true)
+                .isAllowLowerLetters(true)
+                .passwordLength(PASSWORD_LENGTH)
+                .build();
     }
 
     @Bean
