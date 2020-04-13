251fa699294e0c126e48cde0224ecc7f75c3f1c8
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-13 19:28:16 +0300

    change repository interface back to jpa

--- src/main/java/com/robosh/ejournal/data/repository/SchoolRepository.java
+++ src/main/java/com/robosh/ejournal/data/repository/SchoolRepository.java
@@ -3,8 +3,8 @@ package com.robosh.ejournal.data.repository;
 import com.robosh.ejournal.data.entity.School;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SchoolRepository extends PagingAndSortingRepository<School, Long> {
+public interface SchoolRepository extends JpaRepository<School, Long> {
     Page<School> findAll(Pageable pageable);
 }
