61a4dca1b75753007feb752ec21a37a8e335b337
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-04-22 19:53:40 +0300

    change table inserting from java code to sql statements

--- src/test/java/com/robosh/ejournal/service/integration/SchoolServiceTest.java
+++ src/test/java/com/robosh/ejournal/service/integration/SchoolServiceTest.java
@@ -1,19 +1,16 @@
 package com.robosh.ejournal.service.integration;
 
-import com.robosh.ejournal.data.dto.school.SaveSchoolDto;
 import com.robosh.ejournal.data.dto.school.SchoolInfoDto;
-import com.robosh.ejournal.data.entity.SettlementType;
 import com.robosh.ejournal.service.SchoolService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.robosh.ejournal.data.DummyData.ANY_STRING;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
@@ -25,29 +22,12 @@ class SchoolServiceTest {
     @Autowired
     private SchoolService schoolService;
 
+    @Sql("classpath:schoolData.sql")
     @Test
     void should_returnCorrectNumberOfEntities_When_GivenPageable(){
-        for(SaveSchoolDto school : getSaveSchoolDtos(PAGE_SIZE * 3 - 1)){
-            schoolService.save(school);
-        }
         List<SchoolInfoDto> schools = schoolService.findPaginated(0, PAGE_SIZE);
         assertEquals(PAGE_SIZE, schools.size());
         schools = schoolService.findPaginated(2, PAGE_SIZE);
         assertEquals(PAGE_SIZE - 1, schools.size());
-    }
-
-    private List<SaveSchoolDto> getSaveSchoolDtos(int number){
-        List<SaveSchoolDto> saveSchoolDtos = new ArrayList<>();
-        for(int i = 0; i < number; i++){
-            saveSchoolDtos.add(SaveSchoolDto.builder()
-                    .address(ANY_STRING)
-                    .name(ANY_STRING)
-                    .region(ANY_STRING)
-                    .settlementName(ANY_STRING)
-                    .settlementType(SettlementType.CITY)
-                    .url(ANY_STRING)
-                    .build());
-        }
-        return saveSchoolDtos;
     }
 }
---
+++ src/test/resources/schoolData.sql
@@ -0,0 +1,11 @@
+INSERT INTO `school`
+        (id, name, region, url, director_id, address, settlement_name, settlement_type)
+VALUES
+       (1,'name','region','url',NULL,'address','settlement',0),
+       (2,'name','region','url',NULL,'address','settlement',0),
+       (3,'name','region','url',NULL,'address','settlement',0),
+       (4,'name','region','url',NULL,'address','settlement',0),
+       (5,'name','region','url',NULL,'address','settlement',0),
+       (6,'name','region','url',NULL,'address','settlement',0),
+       (7,'name','region','url',NULL,'address','settlement',0),
+       (8,'name','region','url',NULL,'address','settlement',0);
