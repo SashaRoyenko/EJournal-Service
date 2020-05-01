f582e48e9463700681fa934bccdc6e397f2b5c59
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-05-01 11:35:58 +0300

    add find by id endpoint for teacher

--- src/main/java/com/robosh/ejournal/controller/TeacherController.java
+++ src/main/java/com/robosh/ejournal/controller/TeacherController.java
@@ -6,6 +6,8 @@ import com.robosh.ejournal.service.TeacherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,10 @@ public class TeacherController {
     @PostMapping
     public ResponseEntity<TeacherDto> saveTeacher(@RequestBody @Valid SaveTeacherDto teacher) {
         return new ResponseEntity<>(teacherService.save(teacher), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public TeacherDto findTeacherById(@PathVariable Long id) {
+        return teacherService.findById(id);
     }
 }
--- src/main/java/com/robosh/ejournal/service/TeacherService.java
+++ src/main/java/com/robosh/ejournal/service/TeacherService.java
@@ -5,6 +5,7 @@ import com.robosh.ejournal.data.dto.teacher.TeacherDto;
 import com.robosh.ejournal.data.entity.Teacher;
 import com.robosh.ejournal.data.mapping.TeacherMapper;
 import com.robosh.ejournal.data.repository.TeacherRepository;
+import com.robosh.ejournal.exception.ResourceNotFoundException;
 import com.robosh.ejournal.util.PasswordGenerator;
 import com.robosh.ejournal.util.validation.ValidatorProcessor;
 import lombok.RequiredArgsConstructor;
@@ -34,9 +35,18 @@ public class TeacherService {
         return teacherMapper.fromTeacherToTeacherDto(teacher);
     }
 
+    public TeacherDto findById(Long id) {
+        return teacherMapper.fromTeacherToTeacherDto(findTeacherById(id));
+    }
+
     private void fixMapping(SaveTeacherDto dto, Teacher teacher) {
         if (dto.getGroupId() == null) {
             teacher.setGroup(null);
         }
+    }
+
+    private Teacher findTeacherById(Long id) {
+        return teacherRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Teacher", "id", id));
     }
 }
