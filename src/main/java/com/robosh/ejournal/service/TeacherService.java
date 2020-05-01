3ccb18f524d7465ec864e6a94cc5918427668c21
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2020-05-01 10:18:59 +0300

    add dependencies for teacher service

--- src/main/java/com/robosh/ejournal/service/TeacherService.java
+++ src/main/java/com/robosh/ejournal/service/TeacherService.java
@@ -1,8 +1,19 @@
 package com.robosh.ejournal.service;
 
+import com.robosh.ejournal.data.mapping.TeacherMapper;
+import com.robosh.ejournal.data.repository.TeacherRepository;
+import com.robosh.ejournal.util.PasswordGenerator;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class TeacherService {
+
+    private final TeacherRepository teacherRepository;
+    private final PasswordGenerator passwordGenerator;
+    private final ModelMapper modelMapper;
+    private final TeacherMapper teacherMapper;
 
 }
